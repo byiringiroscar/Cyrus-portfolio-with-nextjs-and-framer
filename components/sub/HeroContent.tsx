31530039cd7f6242bdb3ea5321382262002026fe
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-08 23:56:56 +0200

    wip on home

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -4,8 +4,17 @@ import { motion } from 'framer-motion'
 const HeroContent = () => {
   return (
     <motion.div
-    
+    initial='hidden'
+    animate='visible'
+    className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
+        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+            <motion.div
+            variants={}
+            >
+
+            </motion.div>
+        </div>
 
     </motion.div>
   )
