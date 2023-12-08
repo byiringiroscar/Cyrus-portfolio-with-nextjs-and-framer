2655cd89be84a8c2b29f76ec457bf4105cbd351e
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-08 14:37:36 +0200

    Add Hero component and public

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Hero from '@/components/main/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
+    <main className='h-full w-full'>
+        <div className='flex flex-col h-[850px] gap-20'>
+            <Hero />
+        </div>
+    </main>
   )
 }
