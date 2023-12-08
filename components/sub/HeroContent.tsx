91caf4b6cc5cea234d66e15729d8177e0600fd03
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-08 14:43:23 +0200

    Create HeroContent.tsx

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+ 
+const HeroContent = () => {
+  return (
+    <motion.div
+    
+    >
+
+    </motion.div>
+  )
+}
+
+export default HeroContent
