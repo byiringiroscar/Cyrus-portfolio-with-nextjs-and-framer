4ccb2627b3f35ac8efd41b52e49850a0a58eba14
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-10 12:13:41 +0200

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromTop } from '@/utils/motion'
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
  
 const HeroContent = () => {
@@ -20,6 +20,14 @@ const HeroContent = () => {
               <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
                 <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
 
+            </motion.div>
+            <motion.div
+            variants={slideInFromLeft(0.5)}
+            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+            >
+              Providing
+              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
+              project experience
             </motion.div>
         </div>
 
