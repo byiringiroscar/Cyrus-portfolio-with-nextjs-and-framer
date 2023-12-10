8065e6ccbcddb65ad110d9f5d85b0f3d84d390f7
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-10 14:30:15 +0200

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -24,11 +24,13 @@ const HeroContent = () => {
             </motion.div>
             <motion.div
             variants={slideInFromLeft(0.5)}
-            className='flex flex-col gap-6 mt-6 text-6xl text-bold text-white max-w-[600px] w-auto h-auto'
+            className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
             >
-              Providing
-              <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'>the best</span>
-              project experience
+              <span>
+                Providing
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> the best </span>
+                project experience
+              </span>
             </motion.div>
             <motion.p
             variants={slideInFromLeft(0.8)}
