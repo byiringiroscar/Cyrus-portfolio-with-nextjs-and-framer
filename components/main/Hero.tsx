f26cb7fc838306c97a8af86630fc5d530c2b95d2
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-10 11:12:39 +0200

    wip on Herocontent

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeroContent from '../sub/HeroContent'
 
 const Hero = () => {
   return (
@@ -11,6 +12,7 @@ const Hero = () => {
              >
                 <source src='/blackhole.webm' type='video/webm' />
             </video>
+            <HeroContent />
     </div>
   )
 }
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { motion } from 'framer-motion'
 import { slideInFromTop } from '@/utils/motion'
@@ -13,10 +15,10 @@ const HeroContent = () => {
         <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
             <motion.div
             variants={slideInFromTop}
-            className='welcome-box py-[150px] px-[4px] border-[#7042f88b] opacity-[0.9]' 
+            className='Welcome-box py-[8px] px-[4px] border-[#7042f88b] opacity-[0.9]' 
             >
               <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-                <h1 className='welcome-text text-[13px]'>Fullstack Developer Portfcolio</h1>
+                <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
 
             </motion.div>
         </div>
