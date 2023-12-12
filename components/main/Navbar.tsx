18c3115d916aabb5505e99f5abe30571094c8dd5
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-12 16:13:59 +0200

    wip on nav

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,6 +4,19 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+        <div className='w-full h-full flex flex-row items-center justify-between m-auto px-[10px]'>
+            <a href='#about-me' className='h-auto w-auto flex flex-row items-center'>
+                    <Image 
+                        src='/NavLogo.png'
+                        alt='logo'
+                        width={70}
+                        height={70}
+                        className='cursor-pointer hover:animate-slowspin'
+                    />
+                    <span className='font-bold ml-[10px] hidden md:block text-gray-300'>WebChain</span>
+            </a>
+
+        </div>
     </div>
   )
 }
