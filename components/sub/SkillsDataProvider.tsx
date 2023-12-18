375d584461d867a528ee2891bb15ffb897c088a6
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 11:57:37 +0200

    Update SkillsDataProvider.tsx

--- components/sub/SkillsDataProvider.tsx
+++ components/sub/SkillsDataProvider.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+'use client'
 
-const SkillsDataProvider = () => {
+import React from 'react'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
+interface Props {
+    src: string;
+    width: number;
+    height: number;
+    index: number;
+}
+
+const SkillsDataProvider = ({ src, width, height, index} : Props) => {
+    const {ref, inView} = useInView({
+        triggerOnce: true
+    })
+
+    const imageVariants = {
+        hidden: {opacity: 0},
+        visible: {opacity: 1}
+    }
+
+    const animationDelay = 0.3
   return (
-    <div>SkillsDataProvider</div>
+    <motion.div
+    ref={ref}
+    initial="hidden"
+    variants={imageVariants}
+    animate={inView ? "visible" : "hidden"}
+    custom={index}
+    transition={{delay: index * animationDelay}}
+    >
+
+    </motion.div>
   )
 }
 
