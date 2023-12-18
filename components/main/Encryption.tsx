de9b3321afe03b058694c0b995b629c4225af7cf
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 16:02:02 +0200

    Wip on enc

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -11,8 +11,14 @@ const Encryption = () => {
         <div className="absolute w-auto h-auto top-0 z-[5]">
             <motion.div
             variants={slideInFromTop}
+            className="text-[40px] font-medium text-gray-200"
             >
-
+                Performance
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+                {" "}
+                &{" "}
+                </span>
+                Security
             </motion.div>
         </div>
 
