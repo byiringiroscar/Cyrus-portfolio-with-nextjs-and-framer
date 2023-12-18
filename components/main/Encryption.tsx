f89d44c08220c1c0c801d1baaa1e38a4937add2d
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 16:16:31 +0200

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -23,7 +23,20 @@ const Encryption = () => {
         </div>
         <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
           <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-
+                  <Image
+                    src="/LockTop.png"
+                    alt="Lock top"
+                    width={50}
+                    height={50}
+                    className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11"
+                  />
+                  <Image
+                    src="/LockMain.png"
+                    alt="Lock Main"
+                    width={70}
+                    height={70}
+                    className=" z-10"
+                  />
             </div>
 
         </div>
