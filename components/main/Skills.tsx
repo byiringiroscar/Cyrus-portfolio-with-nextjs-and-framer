a6cc0a3aadb70351f53075fe5b28ec69ca60a71c
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 14:45:05 +0200

    Add text skills

--- components/main/Skills.tsx
+++ components/main/Skills.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Backend_skill, Frontend_skill, Full_stack, Other_skill, Skill_data } from '@/constants'
 import SkillsDataProvider from '../sub/SkillsDataProvider'
+import SkillText from '../sub/SkillText'
 
 const Skills = () => {
   return (
@@ -9,6 +10,7 @@ const Skills = () => {
     className='flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden py-20'
     style={{transform: "scale(0.9)"}}
     >
+      <SkillText />
       <div className='flex flex-row justify-around flex-wrap mt-4 gap-5 items-center'>
           {Skill_data.map((image, index) => (
             <SkillsDataProvider
