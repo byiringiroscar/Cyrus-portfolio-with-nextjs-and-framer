7679423b70092d0a0c287b3687fd631b3f47c775
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 23:00:04 +0200

    wip on footer

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -11,7 +11,11 @@ import { FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+        <div className="w-full flex flex-col items-center justify-center m-auto">
+
+        </div>
+    </div>
   )
 }
 
