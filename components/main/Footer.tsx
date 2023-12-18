31270a6d6e5f485b2dbc2045bc685f9339c4bb6c
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-18 22:57:57 +0200

    Create Footer.tsx

---
+++ components/main/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import {
+  RxDiscordLogo,
+  RxGithubLogo,
+  RxInstagramLogo,
+  RxTwitterLogo,
+  RxLinkedinLogo,
+} from "react-icons/rx";
+
+import { FaYoutube } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div>Footer</div>
+  )
+}
+
+export default Footer
